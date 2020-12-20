a21ff36ed0779eaf4af79e624187faf7bf32ef93
nikitaselivyorstov <nikitaseliverstov@gmail.com>
2020-12-20 15:33:02 +0300

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Nikita Seliverstov on 20.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
